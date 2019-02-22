075db24f9bf81e07769f3f043b2a64a5ba865f4f
rtonn <rtonn09umn@gmail.com>
2019-02-21 22:41:49 -0600

    final

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# liri-node-app
-LIRI Bot
+## liri-node-app
+##LIRI Bot
+
+# ABOUT THE APP
+*LIRI is a Language Interpretation and Recognition Interface. 
+* LIRI is a command line node app that takes in parameters and gives back data. 
+* The user has the option of using four commands (listed below) in conjuntion with specific parameters associated with the commands. The  `Commands` are:
+
+   * `concert-this`
+   * `spotify-this-song`
+   * `movie-this`
+   * `do-what-it-says`
+
+# Instruction: 
+1. Open your terminal such as Bash.
+2. Navigate to the folder that contains the `liri.js` file. 
+3. Run the options below. Depending on the command you run, the output will vary:
+    * Run the `concert-this` command "node liri.js concert-this <name of artist or band>". 
+    * Run the `spotify-this-song` command "node liri.js spotify-this-song <name of song>".
+    * Run the `movie-this` commandq "node liri.js movie-this <name of movie>".    
+    * Run the `do-what-it-says` command "node liri.js do-what-it-says".  
+
+# TECHNOLOGIES USED
+  * Javascript
+  * Nodejs
+  * Node packages:
+    * Node-Spotify-API
+    * Request
+    * Moment
+    * DotEnv
+  * APIs used:
+    * Bands in Town
+    * OMDB
+  * Git
+  * GitHub
+
+
--- liri.js
+++ liri.js
@@ -11,10 +11,10 @@ var spotify = new Spotify(keys.spotify);
 var userOption = process.argv[2]; 
 var inputParameter = process.argv[3];
 
-//user input function
+//cmd line user input function........................
 UserInputs(userOption, inputParameter); 
 
-//functions
+//switch functions
 function UserInputs (userOption, inputParameter){ 
     switch (userOption){        
     case 'concert-this':
@@ -30,7 +30,7 @@ function UserInputs (userOption, inputParameter){
         break; 
     
     case 'do-what-it-says':
-        showSomeInfo(); 
+        showInfo(); 
         break; 
     
     default:
@@ -39,7 +39,7 @@ function UserInputs (userOption, inputParameter){
     }
 }
 
-//movie OMDB
+//movie OMDB request.............................
 function showMovieInfo(inputParameter){
     if (inputParameter === undefined) {
             inputParameter = "Jaws"
@@ -50,8 +50,7 @@ function showMovieInfo(inputParameter){
             fs.appendFileSync("log.txt", "If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/" +"\n");
             console.log("It's on Netflix!");
             fs.appendFileSync("log.txt", "It's on Netflix!\n");
-    }              
-    
+    } 
 
     var queryUrl = "http://www.omdbapi.com/?t=" + inputParameter + "&y=&plot=short&apikey=52445c39";
     request(queryUrl, function(error, response, body){        
@@ -81,3 +80,74 @@ function showMovieInfo(inputParameter){
                 }
 });}
 
+//Spotify..........................................
+function showSongInfo(inputParameter) {
+    if (inputParameter === undefined) {
+        inputParameter = "Tusk"; 
+    }
+    spotify.search( {
+        type: "track",
+        query: inputParameter,  
+    }, 
+    function(err,data) {
+        if (err) {
+            console.log("Error occurred: " + err);
+            return;
+        }
+        var songs = data.tracks.items; 
+        for (var i = 0;  i < songs.length; i++) {
+            console.log("****Spotify Song Ingo****"); 
+            fs.appendFileSync("log.txt", "****SPOTIFY SONG INFO****\n");
+            console.log(i);
+            fs.appendFileSync("log.txt", i +"\n");
+            console.log("Song name: " + songs[i].name);
+            fs.appendFileSync("log.txt", "song name: " + songs[i].name +"\n");
+            console.log("Preview song: " + songs[i].preview_url);
+            fs.appendFileSync("log.txt", "preview song: " + songs[i].preview_url +"\n");
+            console.log("Album: " + songs[i].album.name);
+            fs.appendFileSync("log.txt", "album: " + songs[i].album.name + "\n");
+            console.log("Artist(s): " + songs[i].artists[0].name);
+            fs.appendFileSync("log.txt", "artist(s): " + songs[i].artists[0].name + "\n");
+            console.log("*****************************");  
+            fs.appendFileSync("log.txt", "*****************************\n");
+            
+        }
+
+});}; 
+
+//Bands In Town..................................
+function showConcertInfo(inputParameter){
+    var queryUrl = "https://rest.bandsintown.com/artists/" + inputParameter + "/events?app_id=codingbootcamp";
+    
+    request(queryUrl, function(error, response, body) {
+        if (!error && response.statusCode === 200) {
+            var concerts = JSON.parse(body);
+        
+            for (var i = 0; i < concerts.length; i++) {
+                console.log("****Bands in Town Events****"); 
+                fs.appendFileSync("log.txt", "****EVENT INFO****\n");
+                console.log(i);
+                fs.appendFileSync("log.txt", i+"\n");
+                console.log("Name of the Venue: " + concerts[i].venue.name);
+                fs.appendFileSync("log.txt", "Name of the Venue: " + concerts[i].venue.name+"\n");
+                console.log("Venue Location: " +  concerts[i].venue.city);
+                fs.appendFileSync("log.txt", "Venue Location: " +  concerts[i].venue.city+"\n");
+                console.log("Date of the Event: " +  concerts[i].datetime);
+                fs.appendFileSync("log.txt", "Date of the Event: " +  concerts[i].datetime+"\n");
+                console.log("*****************************");
+                fs.appendFileSync("log.txt", "*****************************"+"\n");
+            }
+        }   else{
+            console.log("error"); 
+        }
+}); }
+
+function showInfo(){
+	fs.readFile('random.txt', 'utf8', function(err, data){
+        console.log(data);
+        var txt = data.split(","); 
+
+        showSongInfo(txt[1]); 
+        
+    });
+}
